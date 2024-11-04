f7329a805329f50f4a0831ac5feb80caa609390a
Diego <diegopinlac@gmail.com>
2024-11-04 10:54:16 -0800

    Add Second Onboarding

--- app/src/main/java/com/example/healthconnect/OnboardingStep2Activity.java
+++ app/src/main/java/com/example/healthconnect/OnboardingStep2Activity.java
@@ -1,9 +1,7 @@
 package com.example.healthconnect;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -14,7 +12,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OnboardingStep1Activity extends AppCompatActivity {
+public class OnboardingStep2Activity extends AppCompatActivity {
 
     private ImageButton btnNext;
     private TextView txtSkip;
@@ -22,14 +20,14 @@ public class OnboardingStep1Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_onboarding_step1);
+        setContentView(R.layout.activity_onboarding_step2);
 
         btnNext = (ImageButton) findViewById(R.id.img_onboardingBtnNext);
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(OnboardingStep1Activity.this, OnboardingStep2Activity.class));
                 finish();
+                startActivity(new Intent(OnboardingStep2Activity.this, OnboardingStep3Activity.class));
             }
         });
 
@@ -37,8 +35,8 @@ public class OnboardingStep1Activity extends AppCompatActivity {
         txtSkip.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(OnboardingStep1Activity.this, OnboardingStep3Activity.class));
                 finish();
+                startActivity(new Intent(OnboardingStep2Activity.this, OnboardingStep3Activity.class));
             }
         });
     }
--- app/src/main/java/com/example/healthconnect/SplashScreenActivity.java
+++ app/src/main/java/com/example/healthconnect/SplashScreenActivity.java
@@ -1,7 +1,10 @@
 package com.example.healthconnect;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Timer;
@@ -9,21 +12,27 @@ import java.util.TimerTask;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
+    private static final String PREFS_NAME = "OnboardingProcedures";
+    private static final String KEY_IS_FIRST_RUN = "isFirstRun";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
 
-        TimerTask task = new TimerTask() {
-            @Override
-            public void run() {
-                finish(); // end the life cycle of the SplashActivity by closing the activity
-                //startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
-                startActivity(new Intent(SplashScreenActivity.this, OnboardingStep1Activity.class));
-            }
-        };
+        // Check if it's the first run
+        SharedPreferences preferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        boolean isFirstRun = preferences.getBoolean(KEY_IS_FIRST_RUN, true);
 
-        Timer opening = new Timer();
-        opening.schedule(task, 3000);
+        // Set up a delay if it's the first run
+        new Handler().postDelayed(() -> {
+            if(isFirstRun){
+                startActivity(new Intent(SplashScreenActivity.this, OnboardingStep1Activity.class));
+                preferences.edit().putBoolean(KEY_IS_FIRST_RUN, false).apply();
+            } else {
+                startActivity(new Intent(SplashScreenActivity.this, OnboardingStep3Activity.class));
+            }
+            finish();
+        }, 3000);
     }
 }
