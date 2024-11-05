d5d6a05e9854dbb40253336a3d359bc9b47debb2
Diego <diegopinlac@gmail.com>
2024-11-05 01:50:00 -0800

    FEAT: Add User Registration. Able to insert to database

---
+++ app/src/main/java/com/example/healthconnect/MyDatabaseHelper.java
@@ -0,0 +1,78 @@
+package com.example.healthconnect;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.provider.SyncStateContract;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+
+    private Context context;
+    private static final String DATABASE_NAME = "HealthConnect.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private static final String TABLE_NAME = "user";
+    private static final String COLUMN_ID = "user_id";
+    private static final String COLUMN_USERNAME = "user_name";
+    private static final String COLUMN_USERPASS = "user_pass";
+    private static final String COLUMN_USERROLE = "user_role";
+    private static final String COLUMN_EMAIL= "user_email";
+    private static final String COLUMN_FIRSTNAME = "user_firstName";
+    private static final String COLUMN_LASTNAME = "user_lastName";
+    private static final String COLUMN_DATEOFBIRTH = "date_of_birth";
+    private static final String COLUMN_DATECREATED = "date_created";
+    private static final String COLUMN_DATEUPDATED = "date_updated";
+
+    public MyDatabaseHelper(@Nullable Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query =
+                "CREATE TABLE " + TABLE_NAME + " ( " +
+                        COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_USERNAME + " TEXT NOT NULL, " +
+                        COLUMN_USERPASS + " TEXT NOT NULL, " +
+                        COLUMN_USERROLE + " TEXT, " +
+                        COLUMN_EMAIL + " TEXT, " +
+                        COLUMN_FIRSTNAME + " TEXT, " +
+                        COLUMN_LASTNAME + " TEXT, " +
+                        COLUMN_DATEOFBIRTH + " TEXT, " +
+                        COLUMN_DATECREATED + " TEXT, " +
+                        COLUMN_DATEUPDATED + " TEXT " +
+                        " );";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+    }
+
+    void addUser(String user_name, String user_pass, String user_role, String user_email, String user_firstname, String user_lastname, String date_of_birth, String date_created){
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(COLUMN_USERNAME, user_name);
+        cv.put(COLUMN_USERPASS, user_pass);
+        cv.put(COLUMN_USERROLE, user_role);
+        cv.put(COLUMN_EMAIL, user_email);
+        cv.put(COLUMN_FIRSTNAME, user_firstname);
+        cv.put(COLUMN_LASTNAME, user_lastname);
+        cv.put(COLUMN_DATEOFBIRTH, date_of_birth);
+        cv.put(COLUMN_DATECREATED, date_created);
+
+        long result = db.insert(TABLE_NAME, null, cv);
+        if(result == -1){
+            Toast.makeText(context, "User Registration Failed!", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, "Successfully Registered!", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
--- app/src/main/java/com/example/healthconnect/SignUpActivity.java
+++ app/src/main/java/com/example/healthconnect/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.example.healthconnect;
 
 import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -13,18 +15,57 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class SignUpActivity extends AppCompatActivity {
 
-    EditText txtDateOfBirth;
+    EditText txtUserName, txtUserEmail, txtUserPass, txtUserConfirmPass, txtUserFirstName, txtUserLastName, txtDateOfBirth;
+    Button btnSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        txtUserName = (EditText) findViewById(R.id.txtUserName);
+        txtUserEmail = (EditText) findViewById(R.id.txtEmail);
+        txtUserPass = (EditText) findViewById(R.id.txtUserPass);
+        txtUserConfirmPass = (EditText) findViewById(R.id.txtUserConfirmPass);
+        txtUserFirstName = (EditText) findViewById(R.id.txtUserFirstName);
+        txtUserLastName = (EditText) findViewById(R.id.txtUserLastName);
+
         txtDateOfBirth = findViewById(R.id.txtDateOfBirth);
         txtDateOfBirth.setOnClickListener(v -> showDatePickerDialog());
+
+        // Get current date and time
+        Date currentDate = new Date();
+
+        // Format date as yyyy-MM-dd
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+        String formattedDate = sdf.format(currentDate);
+
+        btnSignUp = (Button) findViewById(R.id.onboarding3_signup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (txtUserPass.getText().toString().equals(txtUserConfirmPass.getText().toString())) {
+                    MyDatabaseHelper myDB = new MyDatabaseHelper(SignUpActivity.this);
+                    myDB.addUser(
+                            txtUserName.getText().toString().trim(),
+                            txtUserPass.getText().toString().trim(),
+                            "Doctor",
+                            txtUserEmail.getText().toString().trim(),
+                            txtUserFirstName.getText().toString().trim(),
+                            txtUserLastName.getText().toString().trim(),
+                            txtDateOfBirth.getText().toString().trim(),
+                            formattedDate
+                    );
+                } else {
+                    Toast.makeText(SignUpActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void showDatePickerDialog() {
@@ -38,7 +79,7 @@ public class SignUpActivity extends AppCompatActivity {
                     Calendar selectedDate = Calendar.getInstance();
                     selectedDate.set(year1, monthOfYear, dayOfMonth);
 
-                    SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy", getResources().getConfiguration().locale);
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", getResources().getConfiguration().locale);
                     String formattedDate = sdf.format(selectedDate.getTime());
 
                     txtDateOfBirth.setText(formattedDate);
